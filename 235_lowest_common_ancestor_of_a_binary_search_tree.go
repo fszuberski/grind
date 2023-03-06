116da6843f99a4a905a9edebff699e2ded19dbad
Filip Szuberski <filip.szuberski@gmail.com>
2023-03-06 09:56:08 +0000

    add 235

---
+++ 235_lowest_common_ancestor_of_a_binary_search_tree.go
@@ -0,0 +1,33 @@
+package main
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+func main_235() {
+
+	// [6,2,8,0,4,7,9,null,null,3,5], 2, 8 -> 6
+	// [6,2,8,0,4,7,9,null,null,3,5], 2, 4 -> 2
+	// [2,1], 2, 1 -> 2
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+
+	return root
+}
+
+// Time complexity: O(logn). Searching a binary tree
+// Space complexity: ~O(n). Depth of recursion stack. Worst case -> one long branch
