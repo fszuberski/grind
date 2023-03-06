ba5f272659f13bf961ee4f04014da97af642050c
Filip Szuberski <filip.szuberski@gmail.com>
2023-03-06 14:53:49 +0000

    add 104

---
+++ 104_maximum_depth_of_binary_tree.go
@@ -0,0 +1,31 @@
+package main
+
+import "math"
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// [3,9,20,null,null,15,7] -> 3
+// [1,null,2] -> 2
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	return maxDepthHelper(root, 0)
+}
+
+func maxDepthHelper(root *TreeNode, currentDepth int) int {
+	if root == nil {
+		return currentDepth
+	}
+
+	currentDepth++
+	return int(math.Max(
+		float64(maxDepthHelper(root.Left, currentDepth)),
+		float64(maxDepthHelper(root.Right, currentDepth)),
+	))
+}
