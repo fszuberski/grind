51234defccd574720d3db659d571038829a67264
Filip Szuberski <filip.szuberski@gmail.com>
2023-03-06 08:33:59 +0000

    add 121

---
+++ 121_best_time_to_buy_and_sell_stock.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"math"
+)
+
+func main() {
+	output := maxProfit([]int{7, 1, 5, 3, 6, 4})
+	if output != 5 {
+		log.Fatalf("expected: %d", 5)
+	}
+
+	output = maxProfit([]int{7, 6, 4, 3, 1})
+	if output != 5 {
+		log.Fatalf("expected: %d", 0)
+	}
+}
+
+func maxProfit(prices []int) int {
+	// from the back
+	// var ans, curMax int
+	// for i := len(prices) - 1; i >= 0; i-- {
+	//     if curMax - prices[i] > ans {
+	//         ans = curMax - prices[i]
+	//     }
+
+	//     if prices[i] > curMax {
+	//         curMax = prices[i]
+	//     }
+	// }
+
+	// return ans
+
+	// from the front
+	var ans int
+	curMin := math.MaxInt
+	for i := range prices {
+		if prices[i] < curMin {
+			curMin = prices[i]
+		}
+
+		if prices[i]-curMin > ans {
+			ans = prices[i] - curMin
+		}
+	}
+
+	return ans
+}
+
+// Time complexity: O(n). Only a single pass is needed.
+// Space complexity: O(1). Only two variables are used.
